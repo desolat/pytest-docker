39042989477518f22a8cf070181e58035b49be5c
Christoph Jähnigen <jaehnigen@kieback-peter.de>
2020-05-27 09:41:08 +0200

    chore: install pytest-runner only when needed (https://github.com/pytest-dev/pytest-runner#conditional-requirement)

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
+import sys
 from setuptools import setup
 
+needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
+pytest_runner = ['pytest-runner >= 5.0, <6.0'] if needs_pytest else []
+
 setup(
-    setup_requires=["wheel >= 0.32", "pytest-runner >= 5.0, <6.0"],
+    setup_requires=["wheel >= 0.32"] + pytest_runner,
     entry_points={"pytest11": ["docker = pytest_docker"]},
 )
